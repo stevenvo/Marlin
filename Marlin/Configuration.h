ba4d4f21854c1868f8a5ce932026863572f7ad87
Steven Vo <stevenvo@uber.com>
2021-03-18 22:42:00 -0700

    fix estep

--- Marlin/Configuration.h
+++ Marlin/Configuration.h
@@ -741,7 +741,7 @@
  * Override with M92
  *                                      X, Y, Z, E0 [, E1[, E2...]]
  */
-#define DEFAULT_AXIS_STEPS_PER_UNIT   { 80, 80, 400, 135 }
+#define DEFAULT_AXIS_STEPS_PER_UNIT   { 100, 100, 400, 135 }
 
 /**
  * Default Max Feed Rate (mm/s)
@@ -1127,7 +1127,7 @@
 
 // Travel limits (mm) after homing, corresponding to endstop positions.
 #define X_MIN_POS 0
-#define Y_MIN_POS 0
+#define Y_MIN_POS -25
 #define Z_MIN_POS 0
 #define X_MAX_POS X_BED_SIZE
 #define Y_MAX_POS Y_BED_SIZE
